fixed normalization

common/util.py
@@ -1,14 +1,13 @@
+import numpy as np
 import pandas as pd
 
-def pair_rebase(df: pd.DataFrame, col_1: str, col_2: str) -> pd.Series:
-    df['Norm_x'] = df[col_1]/df.iloc[0].loc[col_1]
-    df['Norm_y'] = df[col_2]/df.iloc[0].loc[col_2]
-
-def pair_rolling_avg(df: pd.DataFrame, col_1: str, col_2: str) -> pd.Series:
-    avg_1 = df[col_1].rolling(30).mean()
-    avg_2 = df[col_2].rolling(30).mean()
-    return avg_1/avg_2
+def norm_pairs(df: pd.DataFrame, col_1: str, col_2: str, days: int = 30) -> pd.Series:
+    change_x = np.ones((1, df.shape[0]))+df[col_1].pct_change().to_numpy()
+    norm_x = pd.Series(change_x[0], index=df.index).rolling(days).apply(lambda s: np.prod(s))
+    change_y = np.ones((1, df.shape[0]))+df[col_2].pct_change().to_numpy()
+    norm_y = pd.Series(change_y[0], index=df.index).rolling(days).apply(lambda s: np.prod(s))
+    return norm_x/norm_y
 
 def flag_outliers(df: pd.DataFrame, col: str, threshold: int) -> pd.Series:
-    return ~df[col].between(df[col].mean()-threshold*df[col].std(), 
+    return ~df[col].between(df[col].mean()-threshold*df[col].std(),
                             df[col].mean()+threshold*df[col].std())