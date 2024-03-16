fe079e15ec1053e83e62bcb15dd88ed9b7d391d5
theshoe1029 <aschueller@zoominternet.net>
2024-03-16 15:07:26 -0700

    sharpe optimizing

--- backtest/sharpe_optimize.py
+++ backtest/sharpe_optimize.py
@@ -1,6 +1,7 @@
 import backtrader as bt
 import numpy as np
 import pandas as pd
+from scipy.optimize import minimize
 import yfinance as yf
 
 
@@ -9,12 +10,33 @@ class SharpeOptimize(bt.Strategy):
     params = dict()
 
     def __init__(self):
-        print(len(self.data))
+        self.m_hist = []
+        self.pf = np.array([1/len(self.datas)]*len(self.datas))
+
+    def next(self):
+        self.m_hist.append([self.datas[i][0] for i in range(len(self.datas))])
+        if len(self) > 24:
+            self.optimize_shrp()
+        for i, w in enumerate(self.pf):
+            stake = self.broker.get_cash()*w//self.datas[i][0]
+            self.buy(self.datas[i], size=stake)
+
+    def shrp(self, return_rates, w):
+        R = np.matmul(return_rates.to_numpy(), w)
+        return np.sqrt(12)*(R.mean()-self._RISK_FREE)/R.std()
+
+    def optimize_shrp(self):
+        return_rates = pd.DataFrame(self.m_hist).pct_change().dropna()
+        w0 = self.pf.copy()
+        bounds = [(0, 1) for _ in range(len(w0))]
+        result = minimize(lambda w: -self.shrp(return_rates, w), w0, bounds=bounds, constraints={'type': 'eq', 'fun': lambda x: sum(x)-1})
+        print(-result.fun)
+        self.pf = np.array(result.x)
 
 
 def gen_portfolio(tickers, start, interval):
     index = pd.Series()
-    data = {}
+    data = []
     for tkr in tickers:
         if interval == "1y":
             hist = yf.Ticker(tkr).history(start=start, interval="1mo").iloc[::12]
@@ -26,46 +48,32 @@ def gen_portfolio(tickers, start, interval):
                 print(f"Skipping ticker {tkr} due to insufficient data")
             else:
                 index = hist.index
-                data[tkr] = hist
-    return pd.DataFrame(data, index=index)
+                data.append(hist)
+    return data
 
 
 def init_cerebro(data: list[pd.DataFrame]) -> bt.Cerebro:
     cerebro = bt.Cerebro()
     cerebro.addanalyzer(bt.analyzers.AnnualReturn, _name="anr")
     cerebro.addanalyzer(bt.analyzers.Returns, _name="r")
-
+    for row in data:
+        cerebro.adddata(bt.feeds.PandasData(dataname=row))
     return cerebro
 
 
-def backtest_pair(df_1: pd.DataFrame, df_2: pd.DataFrame, tst_id: any):
-    print(f"--------- Mean reversion {tst_id} ---------")
-    pair_df = create_pair(df_1, df_2)
-    ratio = util.norm_pairs(pair_df, "Close_n", "Close_d")
-
-    cerebro = init_cerebro(df_1, df_2)
-    cerebro.addstrategy(SharpeOptimize)
-    cerebro.addsizer(bt.sizers.FixedSize, stake=250)
-    cerebro.addobserver(bt.observers.Trades)
-
-    run_test(cerebro)
-
-
-def run_test(cerebro: bt.Cerebro):
+def main():
+    pf_sp_500 = gen_portfolio(
+        pd.read_csv("../data/sp500_members.csv").Symbol, "2013-01-01", "1mo"
+    )
+    cerebro = init_cerebro(pf_sp_500)
     cerebro.broker.setcash(100000.0)
+    cerebro.addstrategy(SharpeOptimize)
     results = cerebro.run()
     annual_returns = results[0].analyzers.anr.get_analysis().values()
     anr100 = list(map(lambda x: x * 100, annual_returns))
     returns = results[0].analyzers.r.get_analysis()
     print(f"Returns y/y: {anr100}")
     print(f"Normalized return %: {returns['rnorm100']}")
-
-
-def main():
-    pf_sp_500 = gen_portfolio(
-        pd.read_csv("../data/sp500_members.csv").Symbol, "2010-01-01", "1mo"
-    )
-    pf_sp_500.to_csv("../data/sp500_monthly.csv")
 
 
 if __name__ == "__main__":
